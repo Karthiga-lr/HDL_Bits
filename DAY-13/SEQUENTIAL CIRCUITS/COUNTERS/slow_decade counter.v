411ab27dd80fb2efaa4e71ebc76ffeeb3b0b7afa
Karthiga-lr <lr.karthiga20@gmail.com>
2025-06-23 23:53:59 +0530

    Update slow_decade counter.v

--- DAY-13/SEQUENTIAL CIRCUITS/COUNTERS/slow_decade counter.v
+++ DAY-13/SEQUENTIAL CIRCUITS/COUNTERS/slow_decade counter.v
@@ -1,1 +1,15 @@
-
+module top_module (
+    input clk,
+    input slowena,
+    input reset,
+    output [3:0] q);
+    always@(posedge clk)
+        if(reset)
+            q <= 4'd0;
+    else if (!slowena)
+        q <=q;
+    else if (q == 4'd9)
+        q <=4'd0;
+    else
+        q<=q+4'd1;   
+endmodule
