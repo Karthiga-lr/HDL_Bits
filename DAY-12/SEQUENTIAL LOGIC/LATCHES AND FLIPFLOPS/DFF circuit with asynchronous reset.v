3f9d248e1d86c1c4fc1a15bb5d231d0326576522
Karthiga-lr <lr.karthiga20@gmail.com>
2025-06-23 22:08:59 +0530

    Update DFF circuit with asynchronous reset.v

--- DAY-12/SEQUENTIAL LOGIC/LATCHES AND FLIPFLOPS/DFF circuit with asynchronous reset.v
+++ DAY-12/SEQUENTIAL LOGIC/LATCHES AND FLIPFLOPS/DFF circuit with asynchronous reset.v
@@ -1,1 +1,12 @@
+module top_module (
+    input clk,
+    input d, 
+    input ar,   // asynchronous reset
+    output reg q);
+    always@(posedge clk or posedge ar)
+        if(ar)
+            q <= 1'h0;
+    else
+        q <=d;
+endmodule
 
