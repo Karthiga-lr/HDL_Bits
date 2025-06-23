4bb87997ae3a4600ef184cce3dc5350806278003
Karthiga-lr <lr.karthiga20@gmail.com>
2025-06-23 23:42:01 +0530

    Update Decade_counter.v

--- DAY-13/SEQUENTIAL CIRCUITS/COUNTERS/Decade_counter.v
+++ DAY-13/SEQUENTIAL CIRCUITS/COUNTERS/Decade_counter.v
@@ -1,1 +1,11 @@
+module top_module (
+    input clk,
+    input reset,        // Synchronous active-high reset
+    output reg [3:0] q);
+    always@(posedge clk)
+        if (reset || (q == 4'd9))
+            q<=4'd0;
+    else 
+        q<= q+4'd1;
+endmodule
 
