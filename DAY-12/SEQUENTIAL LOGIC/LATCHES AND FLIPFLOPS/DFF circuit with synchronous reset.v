4fa09d6c650c8c0f064ebc7bc23c14a2f07aac23
Karthiga-lr <lr.karthiga20@gmail.com>
2025-06-23 22:10:02 +0530

    Update DFF circuit with synchronous reset.v

--- DAY-12/SEQUENTIAL LOGIC/LATCHES AND FLIPFLOPS/DFF circuit with synchronous reset.v
+++ DAY-12/SEQUENTIAL LOGIC/LATCHES AND FLIPFLOPS/DFF circuit with synchronous reset.v
@@ -1,1 +1,12 @@
+module top_module (
+    input clk,
+    input d, 
+    input r,   // synchronous reset
+    output q);
+    always@(posedge clk)
+    if(r)
+        q<=1'h0;
+    else
+        q<=d;
+endmodule
 
