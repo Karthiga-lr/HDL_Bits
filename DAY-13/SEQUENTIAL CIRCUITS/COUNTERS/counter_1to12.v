30d40c19850bdf72a958e7c537cb274d0caac858
Karthiga-lr <lr.karthiga20@gmail.com>
2025-06-24 19:35:19 +0530

    Update counter_1to12.v

--- DAY-13/SEQUENTIAL CIRCUITS/COUNTERS/counter_1to12.v
+++ DAY-13/SEQUENTIAL CIRCUITS/COUNTERS/counter_1to12.v
@@ -1,1 +1,20 @@
-
+module top_module (
+    input clk,
+    input reset,
+    input enable,
+    output [3:0] Q,
+    output c_enable,
+    output c_load,
+    output [3:0] c_d
+); //
+    wire [3:0] count4_Q; 
+    assign Q = count4_Q;
+    assign c_enable = enable;
+    
+    assign c_load = ((count4_Q == 4'd12 && c_enable)|| reset); 
+    assign c_d = 4'd1;
+    
+    
+    count4 the_counter (clk, c_enable, c_load, c_d, count4_Q);
+    
+endmodule
