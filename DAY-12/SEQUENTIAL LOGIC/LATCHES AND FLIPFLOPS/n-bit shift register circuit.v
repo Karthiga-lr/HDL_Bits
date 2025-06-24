a37ba8dd77bde9cea8c3b9d94aa4270e734810f3
Karthiga-lr <lr.karthiga20@gmail.com>
2025-06-24 17:15:37 +0530

    Update n-bit shift register circuit.v

--- DAY-12/SEQUENTIAL LOGIC/LATCHES AND FLIPFLOPS/n-bit shift register circuit.v
+++ DAY-12/SEQUENTIAL LOGIC/LATCHES AND FLIPFLOPS/n-bit shift register circuit.v
@@ -1,1 +1,15 @@
+module top_module (
+    input clk,
+    input w, R, E, L,
+    output reg Q
+);
+    always@(posedge clk)
+        if(L)
+            Q<=R;
+    else if(E)
+        Q<=w;
+    else
+        Q<=Q;
+
+endmodule
 
