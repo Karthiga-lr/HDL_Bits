908c0d9133d379b139517e26f8521675d4cd9ec8
Karthiga-lr <lr.karthiga20@gmail.com>
2025-07-04 14:02:16 +0530

    Update testbench2.v

--- DAY-20/VERIFICATION: WRITING TESTBENCHES/testbench2.v
+++ DAY-20/VERIFICATION: WRITING TESTBENCHES/testbench2.v
@@ -1,1 +1,29 @@
+module top_module();
+    reg clk;
+    reg in;
+    reg [2:0] s;
+    wire out;
+    
+    q7 dut (.clk(clk),.in(in),.s(s),.out(out));
+    
+    initial begin
+        clk=0;
+        in=0;
+        s=2;
+        #10;
+        s=6;
+        #10;
+        in = 1;
+        s=2;
+        #10;
+        in=0;
+        s=7;
+        #10;
+        in=1;
+        s=0;
+        #30;
+        in=0;
+    end
+    always#5 clk =~clk; 
+endmodule
 
