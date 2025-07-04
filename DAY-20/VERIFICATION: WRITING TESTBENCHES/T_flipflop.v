d1ec7d11127d219ad02a23e2b94d6e1226e6a2a1
Karthiga-lr <lr.karthiga20@gmail.com>
2025-07-04 14:03:45 +0530

    Update T_flipflop.v

--- DAY-20/VERIFICATION: WRITING TESTBENCHES/T_flipflop.v
+++ DAY-20/VERIFICATION: WRITING TESTBENCHES/T_flipflop.v
@@ -1,1 +1,27 @@
+module top_module ();
 
+    reg clk;
+    reg reset;
+    reg t;
+    wire q;
+    
+    tff dut(.clk(clk),.reset(reset),.t(t),.q(q));
+    initial begin
+        clk=0;
+        reset = 1'b0;
+        #5;
+        reset = 1'b1;
+        #10;
+        reset = 1'b0;
+    end
+    
+    always@(posedge clk)begin
+        if(reset)begin
+            t<=1'b0;
+        end
+        else begin 
+            t<=1'b1;
+        end
+    end     
+    always#5 clk=~clk;      
+endmodule
