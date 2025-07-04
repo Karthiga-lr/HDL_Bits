ac6dbb0504853be5cdd1e8f7374fcdfeb175dfae
Karthiga-lr <lr.karthiga20@gmail.com>
2025-07-04 21:57:00 +0530

    Update Lemmings2.v

--- DAY-16/SEQUENTIAL LOGIC/FINITE STATE MACHINES/Lemmings2.v
+++ DAY-16/SEQUENTIAL LOGIC/FINITE STATE MACHINES/Lemmings2.v
@@ -1,1 +1,12 @@
-
+module top_module(
+    input clk,
+    input areset,    // Freshly brainwashed Lemmings walk left.
+    input bump_left,
+    input bump_right,
+    input ground,
+    output walk_left,
+    output walk_right,
+    output aaah ); 
+    localparam [1:0] WALK_L = 2'b00,WALK_R = 2'b01,
+    FALL_L = 2'b10,
+ 
